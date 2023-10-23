e36f15d18e661a4a1760c185f9d6eb639624ab48
Abubakr <thefox5000@gmail.com>
2023-10-23 13:24:49 +0300

    Delete obj redundant variable

--- app.js
+++ app.js
@@ -78,39 +78,3 @@ app.listen(3000, () => {
     console.log('App running on port 3000');
 })
 
-const obj = {
-    id: 57562,
-    url: 'https://www.tvmaze.com/shows/57562/under-the-vines',
-    name: 'Under the Vines',
-    type: 'Scripted',
-    language: 'English',
-    genres: [ 'Drama', 'Comedy' ],
-    status: 'To Be Determined',
-    runtime: 45,
-    averageRuntime: 45,
-    premiered: '2021-12-06',
-    ended: null,
-    officialSite: 'https://acorn.tv/underthevines/',
-    schedule: { time: '', days: [ 'Monday' ] },
-    rating: { average: 7.1 },
-    weight: 93,
-    network: null,
-    webChannel: {
-      id: 129,
-      name: 'Acorn TV',
-      country: null,
-      officialSite: 'https://acorn.tv/'
-    },
-    dvdCountry: null,
-    externals: { tvrage: null, thetvdb: 401361, imdb: 'tt13612448' },      
-    image: {
-      medium: 'https://static.tvmaze.com/uploads/images/medium_portrait/382/955376.jpg',
-      original: 'https://static.tvmaze.com/uploads/images/original_untouched/382/955376.jpg'
-    },
-    summary: "<p>This dramedy follows two unlikely city slickers who inherit a failing vineyard in rural New Zealand. Despite neither having ever done a hard days' work in their lives and both despising the other, they must somehow make the vineyard successful so they can sell up, split up, and get out of there.</p>",
-    updated: 1675998107,
-    _links: {
-      self: { href: 'https://api.tvmaze.com/shows/57562' },
-      previousepisode: { href: 'https://api.tvmaze.com/episodes/2453695' } 
-    }
-  }
